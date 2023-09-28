06d7968729d93491f9e2247d64cb4a23dfa9e763
Hardik <101271001+HardikSaggar@users.noreply.github.com>
2023-09-28 17:13:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-'use strict';
+"use strict";
 
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.menu');
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".menu");
+const topBtn = document.querySelector(".top-btn");
+const home = document.querySelector("#home");
+
+// Responsive Hamburger Menu
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
 
-document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    navMenu.classList.remove('active');
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
 
-}));
+// Back to top
+topBtn.addEventListener("click", function (e) {
+  if (e.target.classList.contains("fa-circle-up"))
+    home.scrollIntoView({ behavior: "smooth" });
+});
 
+const backTop = (entries) => {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) topBtn.classList.add("active");
+  else topBtn.classList.remove("active");
+};
 
+const observer = new IntersectionObserver(backTop, {
+  root: null,
+  threshold: 0,
+});
+observer.observe(home);
