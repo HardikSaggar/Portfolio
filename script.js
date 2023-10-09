c7cc93f80be50dd165ab32dc80c542c4581c913c
Hardik <hardiksaggar99@gmail.com>
2023-10-09 22:06:03 +0530

    Revert "New updation"
    This reverts commit a92ea559234d4746fbf788d826f2da31559525f1.

--- script.js
+++ script.js
@@ -5,17 +5,11 @@ const btnAbout = document.querySelector(".btn-1");
 const topBtn = document.querySelector(".top-btn");
 const tabBtn = document.querySelectorAll(".tab__operation-btn");
 
-// Navigation
 const navMenu = document.querySelector(".menu");
-const progressBar = document.querySelector("#progress-bar");
-
-// containers
 const tabContainer = document.querySelector(".tab-container");
 const cardContent = document.querySelectorAll(".card__content");
 // Sections
 const sections = document.querySelectorAll(".section");
-const section = document.querySelector(".section");
-const main = document.querySelector('#main')
 const aboutSec = document.querySelector("#about");
 const home = document.querySelector("#home");
 
@@ -31,15 +25,6 @@ document.querySelectorAll(".nav-link").forEach((n) =>
     navMenu.classList.remove("active");
   })
 );
-
-// Progress Bar
-const animateProgressBar = () => {
-  let scrollDistance = -main.getBoundingClientRect().top;
-  let value = Math.floor((scrollDistance/25));
-  console.log(value);
-  progressBar.style.width = value + "%";
-};
-window.addEventListener("scroll", animateProgressBar);
 
 // Button to About section
 btnAbout.addEventListener("click", function () {
